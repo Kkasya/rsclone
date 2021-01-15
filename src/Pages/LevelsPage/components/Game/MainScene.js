ac8d5a06f89d609b56cc6fd444cd6f0db48969af
reagentjs <reagentjs@gmail.com>
2021-01-15 15:49:24 +0300

    feat: add basic interactivity

--- src/Pages/LevelsPage/components/Game/MainScene.js
+++ src/Pages/LevelsPage/components/Game/MainScene.js
@@ -1,7 +1,7 @@
 import * as Phaser from 'phaser';
 import EasyStar from 'easystarjs';
 import gameConstants from './gameConstants';
-import map from './levels/map.json';
+import map from './levels/map4.json';
 import Char from './Char/Char';
 
 export default class MainScene extends Phaser.Scene {
@@ -25,28 +25,49 @@ export default class MainScene extends Phaser.Scene {
 
     const tileset = this.map.addTilesetImage('tiles');
     const layer = this.map
-      .createLayer(0, tileset, 0, 0)
+      .createLayer(0, tileset)
       .setInteractive({
         // cursor: `url(blur.cur), pointer`,
         cursor: 'pointer',
       });
 
-    layer.setCollisionByProperty({ isCollied: true });
-    this.physics.add.collider(layer, this.player);
+    // layer.setCollisionByProperty({ isCollied: true });
+    // this.physics.add.collider(layer, this.char);
 
     const offset = gameConstants.tileSize + gameConstants.tileSize / 2;
-    this.player = new Char(this, offset, offset, 'char');
+    this.char = new Char(this, offset, offset, 'char');
 
     this.input.on('pointerdown', (pointer) => {
       if (pointer.primaryDown) {
         this._createPath(pointer);
       }
     });
+
+    this.objectsLayer = this.map.getObjectLayer('objects')['objects'];
+    this.objects = this.physics.add.staticGroup();
+    this.objectsLayer.forEach(object => {
+      let obj = this.objects.create(object.x, object.y, object.gid);
+      obj.setScale(object.width / 40, object.height / 40);
+      obj.setOrigin(0);
+      obj.body.width = object.width;
+      obj.body.height = object.height;
+    });
+
+    this.physics.add.overlap(this.char, this.objects, this.playInteraction, null, this);
+  }
+
+  playInteraction(player, obj) {
+    obj.destroy(obj.x, obj.y); // remove the tile/coin
+    console.log(obj)
+    console.log('destroyed')
+    // coinScore++; // increment the score
+    // text.setText(`Coins: ${coinScore}x`); // set the text to show the current score
+    // return false;
   }
 
   _createPath(pointer) {
-    const fromX = Math.floor(this.player.x / gameConstants.tileSize);
-    const fromY = Math.floor(this.player.y / gameConstants.tileSize);
+    const fromX = Math.floor(this.char.x / gameConstants.tileSize);
+    const fromY = Math.floor(this.char.y / gameConstants.tileSize);
     const toX = this.map.worldToTileX(pointer.worldX);
     const toY = this.map.worldToTileX(pointer.worldY);
 
@@ -59,12 +80,13 @@ export default class MainScene extends Phaser.Scene {
   _defineAllAndAcceptableTiles() {
     const allTiles = [];
     const acceptableTiles = [];
+    const properties = this.map.tilesets[0].tileProperties;
 
     for (let y = 0; y < this.map.height; y++) {
       const col = [];
       for (let x = 0; x < this.map.width; x++) {
         const id = this.getTileID(x, y);
-        if (id === 1 || id === 22) {
+        if (!properties[id - 1] || !properties[id - 1].isCollied) {
           acceptableTiles.push(id);
         }
         col.push(id);
@@ -96,7 +118,7 @@ export default class MainScene extends Phaser.Scene {
       const cellX = path[i + 1].x;
       const cellY = path[i + 1].y;
       tweens.push({
-        targets: this.player,
+        targets: this.char,
         x: {
           value: cellX * this.map.tileWidth + gameConstants.tileSize / 2,
           duration: gameConstants.duration,
