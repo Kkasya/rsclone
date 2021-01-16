3127cba9b43bcc7857e82bcecb487861d2effcdb
Julia Gvozdeva <yulya.gvozdeva.96@mail.ru>
2021-01-16 18:42:39 +0400

    feat: resolve merge conflict with develop

--- src/Pages/HelpPage/HelpPage.js
+++ src/Pages/HelpPage/HelpPage.js
@@ -2,10 +2,7 @@
 import React from 'react';
 import { Container } from '@material-ui/core';
 import stylesCommon from '../../common/styles/stylesCommon';
-<<<<<<< HEAD
 import HelpContent from './HelpComponents/HelpContent';
-=======
->>>>>>> 6edaa8f788214c858620b3b6b456ff1366f42177
 
 export default function HelpPage() {
   const commonStyles = stylesCommon();
