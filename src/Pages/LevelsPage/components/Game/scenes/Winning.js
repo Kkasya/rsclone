84825d31e15ee5c7b3fc4fc0d974c22ced7e228c
reagentjs <reagentjs@gmail.com>
2021-02-03 11:52:44 +0300

    feat: start implementing localstorage

--- src/Pages/LevelsPage/components/Game/scenes/Winning.js
+++ src/Pages/LevelsPage/components/Game/scenes/Winning.js
@@ -3,12 +3,26 @@ import * as Phaser from 'phaser';
 export default class Winning extends Phaser.Scene {
   constructor() {
     super('Winning');
+    this.once = false;
   }
 
   create() {
     this.add.text(20, 20, 'Congratulations!');
+    if (!this.once) {
+      this.once = true;
+      this._saveProgress();
+      this._redirectToMainScene();
+    }
+  }
+
+  _saveProgress() {
+    localStorage.setItem(`dweep-${this.game.levelNumber}`, true);
+  }
+
+  _redirectToMainScene() {
     setTimeout(() => {
       this.scene.start('MainScene');
+      this.once = false;
     }, 2000);
   }
 }
